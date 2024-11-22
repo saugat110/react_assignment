ef669f35ee17c97ec3816d2356ffa4cf3e9239e5
saugat110 <105158208+saugat110@users.noreply.github.com>
2024-11-22 07:07:40 +0545

    changes2

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef,useState } from 'react'
 
 const Form = ({names, setNames}) => {
   const [isDisabled, setDisabled] = useState(false);
+  const[currName, setCurrName] = useState('');
   const newname =useRef();
 
   const handleOnSubmit = (event) => {
@@ -9,20 +10,20 @@ const Form = ({names, setNames}) => {
 
     if(names.length < 5 && newname.current.value != ''){
       setNames([...names, newname.current.value]);
+      setCurrName(newname.current.value);
       newname.current.value = "";
     }else if(names.length >= 5){
       setDisabled(true);
     }
-
   }
 
   useEffect(() => {
     if(newname){
-      console.log(`Welcome ${newname.current.value}, Hope u have a great day`);
+      console.log(`Welcome ${currName}, Hope u have a great day`);
     }
-    return (() => {
+    return () => {
       console.log("Unmounted");
-    })
+    }
   }, [names]);
 
   return (
